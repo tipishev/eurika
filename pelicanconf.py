4a22fc645e43b0386fdf9c358bf5d7ec4b2d48b2
Timofey Tipishev <timofey.tipishev@kivra.com>
2021-07-30 08:13:33 +0200

    rename theme dir

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,25 +11,11 @@ TIMEZONE = 'Europe/Stockholm'
 
 DEFAULT_LANG = 'ru'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
 DEFAULT_PAGINATION = False
 
 TYPOGRIFY = True
 
-# to keep Main page first in navigation bar
-
-ARTICLE_ORDER_BY = 'basename'
-PAGE_ORDER_BY = 'basename'
-
-#  REVERSE_CATEGORY_ORDER = True
-
 #  THEME = 'notmyidea'
-THEME = 'theme'
+THEME = 'my_theme'
 #  THEME = '/home/user/eurika/pelican-themes/notmyidea-cms'
 #  CSS_FILE = 'wide.css'
