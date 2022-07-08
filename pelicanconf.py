2eb9b7d6941d3ef21453e1ee6dcc826511ac8fa1
Timofey Tipishev <timofey.tipishev@kivra.com>
2022-07-08 20:18:54 +0200

    use github URL

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-SITEURL = 'http://eurika.se'
+SITEURL = 'https://tipishev.github.io/eurika/'
+#  SITEURL = 'http://eurika.se'
 #  SITEURL = 'http://localhost:8000'
 
 AUTHOR = 'admin'
