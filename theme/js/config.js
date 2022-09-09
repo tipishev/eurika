7deca82dae80b63b74cd10ca505f937d394c2c91
tipishev <tipishev@users.noreply.github.com>
2022-09-09 19:31:29 +0000

    [ci skip] Automated deployment to GitHub Pages on 1662751889290

---
+++ theme/js/config.js
@@ -0,0 +1,20 @@
+window._skel_config = {
+	preset: 'standard',
+
+        // TODO populate prefix from pelicanconf
+        prefix: '/eurika/theme/css/style',
+        // prefix: '/theme/css/style',
+
+	resetCSS: true,
+	breakpoints: {
+		'desktop': {
+			grid: {
+				gutters: 50
+			}
+		}
+	}
+};
+
+window._skel_panels_config = {
+	preset: 'standard'
+};
